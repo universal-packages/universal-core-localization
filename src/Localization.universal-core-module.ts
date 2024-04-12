d2bbf09bf3a66245fcb71306dc1f28f214d681df
David De Anda <omarandstuff@gmail.com>
2024-04-11 19:19:50 -0600

    Localization logs

--- src/Localization.universal-core-module.ts
+++ src/Localization.universal-core-module.ts
@@ -1,6 +1,8 @@
 import { CoreModule } from '@universal-packages/core'
 import { Localization, LocalizationOptions } from '@universal-packages/localization'
 
+import { LOG_CONFIGURATION } from './LOG_CONFIGURATION'
+
 export default class LocalizationModule extends CoreModule<LocalizationOptions> {
   public static readonly moduleName = 'localization-module'
   public static readonly description = 'Localization initialization module'
@@ -9,6 +11,14 @@ export default class LocalizationModule extends CoreModule<LocalizationOptions> 
 
   public async prepare(): Promise<void> {
     this.subject = new Localization(this.config)
+
+    this.subject.on('waring', (event) => {
+      this.logger.log({ level: 'WARNING', message: event.message, category: 'LOCALIZATION' }, LOG_CONFIGURATION)
+    })
+
+    this.subject.on('error', (event) => {
+      this.logger.log({ level: 'ERROR', error: event.error, category: 'LOCALIZATION' }, LOG_CONFIGURATION)
+    })
 
     this.subject.prepare()
   }
