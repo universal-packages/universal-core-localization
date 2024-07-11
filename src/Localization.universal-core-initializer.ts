d2b8277dddab76b97641fe65715f10c5696e3fe7
David De Anda <omarandstuff@gmail.com>
2024-07-11 15:07:48 -0600

    Right initializer import

--- src/Localization.universal-core-initializer.ts
+++ src/Localization.universal-core-initializer.ts
@@ -1,4 +1,4 @@
-import CoreInitializer from '@universal-packages/core/CoreInitializer'
+import { CoreInitializer } from '@universal-packages/core'
 
 export default class LocalizationInitializer extends CoreInitializer {
   public static readonly initializerName = 'localization'
